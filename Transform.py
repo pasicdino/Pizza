3201e9d45f522b89c20dfb6eac674a3b02dc1bbb
Dino <dpasic8@gmail.com>
2021-09-22 17:10:54 +0200

    menu print volledig

---
+++ Transform.py
@@ -0,0 +1,7 @@
+def listOfTuplesToList(list):
+    newList = []
+    for i in range(len(list)):
+        newList.append(list[i][0])
+    return newList
+
+
--- mainGUI.py
+++ mainGUI.py
@@ -1,30 +1,21 @@
 import PySimpleGUI as sg
 
-layout = [
-    [sg.Text("Thank you for ordering from us!")],
-    [sg.Button("Pizzas")],
-    [sg.Button("Your Orders")]
-]
+from DBConnector import DBConnector
+db = DBConnector()
 
-window = sg.Window("Dino's Pizzeria", layout)
+amountOfPizzas = len(db.getPizzaNames())
+for i in range(1, amountOfPizzas):
+    print(db.getPizza(i))
+    print(db.getSmallPizzaPrice(i))
+
+amountOfDesserts = len(db.getDesserts())
+for i in range(1, amountOfDesserts+1):
+    print(db.getDessert(i))
+    print(db.getDessertPrice(i))
+
+amountOfDrinks = len(db.getDrinks())
+for i in range(1, amountOfDrinks+1):
+    print(db.getDrink(i))
+    print(db.getDrinkPrice(i))
 
 
-
-pizzaLayout = [
-    [sg.Text("Choose your pizza,")],
-    [sg.Button("Margherita")]
- ]
-
-pizzaWindow = sg.Window("Menu", pizzaLayout)
-
-while True:
-    event, values = window.read()
-    if event == "Your Orders" or event == sg.WIN_CLOSED:
-        break
-    if event == "Pizzas":
-        window.hide()
-        event, values = pizzaWindow.read()
-        if event == "Margherita" or event == sg.WIN_CLOSED():
-            window.un_hide()
-            pizzaWindow.close()
-window.close()
