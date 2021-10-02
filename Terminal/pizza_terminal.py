9cfd455e00b5ecb02d315243ff2366667faffbe1
LaurenceNickel <laurencenickel00@gmail.com>
2021-10-02 22:45:05 +0200

    User can now see current order and delete item (under construction)

--- Terminal/pizza_terminal.py
+++ Terminal/pizza_terminal.py
@@ -4,6 +4,10 @@ import json
 
 BASE_URL = "http://127.0.0.1:5000"
 
+pizza_orders = []
+drink_orders = []
+dessert_orders = []
+
 
 def get_pizza(pizza_id):
     response = requests.get(BASE_URL + "/pizza/" + pizza_id)
@@ -85,6 +89,7 @@ def get_list_of_desserts():
 
 desserts = get_list_of_desserts()
 
+
 def get_list_of_pizzas():
     pizzas = []
     for i in range(1, 11):
@@ -94,11 +99,29 @@ def get_list_of_pizzas():
 pizzas = get_list_of_pizzas()
 
 
+def get_pizza_ordered():
+    pizza_orders.append("Go back")
+    pizza_orders.append(Separator(""))
+    return pizza_orders
+
+
+def get_drink_ordered():
+    drink_orders.append("Go back")
+    drink_orders.append(Separator(""))
+    return drink_orders
+
+
+def get_dessert_ordered():
+    dessert_orders.append("Go back")
+    dessert_orders.append(Separator(""))
+    return dessert_orders
+
+
 start_menu = {
     "type": "list",
     "name": "start",
     "message": "Welcome to Back to the Pizza in Hill Valley. What would you like to do?",
-    "choices": ["See the menu", "See your previous orders", "Place your order", "Quit"],
+    "choices": ["See the menu", "See your current order", "See your previous orders", "Place your order", "Quit"],
 }
 
 
@@ -115,6 +138,41 @@ sizes = {
     "name": "selectSize",
     "message": "Select the size of your pizza.",
     "choices": ["Small", "Medium" + " + " + str(MEDIUM_MINUS_SMALL) + " euros", "Large" + " + " + str(LARGE_MINUS_SMALL) + " euros", Separator(""), "Go back"]
+}
+
+which_kind_of_order = {
+    "type": "list",
+    "name": "kind_of",
+    "message": "Which part of your order would you like to see?",
+    "choices": ["Pizzas", "Drinks", "Desserts", Separator(""), "Go back"]
+}
+
+pizzas_ordered = {
+    "type": "list",
+    "name": "pizzas_ordered",
+    "message": "Here are the pizzas present in your order. Select one to delete it, otherwise select 'go back'",
+    "choices": get_pizza_ordered()
+}
+
+drinks_ordered = {
+    "type": "list",
+    "name": "drinks_ordered",
+    "message": "Here are the drinks present in your order. Select one to delete it, otherwise select 'go back'",
+    "choices": get_drink_ordered()
+}
+
+desserts_ordered = {
+    "type": "list",
+    "name": "desserts_ordered",
+    "message": "Here are the desserts present in your order. Select one to delete it, otherwise select 'go back'",
+    "choices": get_dessert_ordered()
+}
+
+delete_item = {
+    "type": "list",
+    "name": "deletion",
+    "message": "Do you want to delete this item?",
+    "choices": ["Yes", "No"]
 }
 
 validateEmailForOrders = {
@@ -156,36 +214,64 @@ def check_for_menu(menu_item_selected):
     for i in range(1, 5):
         if menu_item_selected.startswith(get_drink_with_price(str(i))):
             print("-----> " + get_drink(str(i)) + " is added to your order!")
-            #TODO: DrinkOrder logic
+            drink_orders.append(get_drink(str(i)))
 
     for i in range(1, 3):
         if menu_item_selected.startswith(get_dessert_with_price(str(i))):
             print("-----> " + get_dessert(str(i)) + " is added to your order!")
-            #TODO: DessertOrder logic
+            dessert_orders.append(get_dessert(str(i)))
 
 
 def check_for_sizes(size_selected, i):
     if size_selected.startswith("Small"):
         print("-----> " + get_pizza(str(i)) + " of size Small is added to your order!")
-        #TODO: PizzaOrder logic
+        pizza_orders.append(get_pizza(str(i)) + " - Small")
     if size_selected.startswith("Medium"):
         print("-----> " + get_pizza(str(i)) + " of size Medium is added to your order!")
-        # TODO: PizzaOrder logic
+        pizza_orders.append(get_pizza(str(i)) + " - Medium")
     if size_selected.startswith("Large"):
         print("-----> " + get_pizza(str(i)) + " of size Large is added to your order!")
-        # TODO: PizzaOrder logic
+        pizza_orders.append(get_pizza(str(i)) + " - Large")
     if size_selected.startswith("Go back"):
         return
 
-def check_for_order_type(order_selected):
-    # Check in database for id.
-    if order_selected == "order":
-        viewOrder()
+
+def check_kind_of_order(kind_selected):
+    if kind_selected == "Pizzas":
+        while True:
+            order_pizza = prompt(pizzas_ordered)
+            pizza_selected = order_pizza["pizzas_ordered"]
+            if pizza_selected == "Go back":
+                break
+            #check_for_selected_pizza(pizza_selected)
+
+    if kind_selected == "Drinks":
+        while True:
+            order_drink = prompt(drinks_ordered)
+            drink_selected = order_drink["drinks_ordered"]
+            if drink_selected == "Go back":
+                break
+            #check_for_selected_drink(drink_selected)
+
+    if kind_selected == "Desserts":
+        while True:
+            order_dessert = prompt(desserts_ordered)
+            dessert_selected = order_dessert["desserts_ordered"]
+            if dessert_selected == "Go back":
+                break
+            #check_for_selected_dessert(pizza_selected)
 
 
+# def check_for_selected_pizza(pizza_selected):
+#     if pizza_selected.startswith(get_pizza_with_price(str(i))):
+#         pizza = prompt(delete_item)
+#         to_be_deleted = pizza["deletion"]
+#         delete_item(to_be_deleted, )
+#
+#
+# def delete_item(menu):
+#     if
 
-def viewOrder():
-    print("orderdetails")
 
 if __name__ == "__main__":
     while True:
@@ -198,6 +284,14 @@ if __name__ == "__main__":
                 if menu_item_selected == "Go back":
                     break
                 check_for_menu(menu_item_selected)
+
+        if answer == "See your current order":
+            while True:
+                kind_of_order = prompt(which_kind_of_order)
+                kind_selected = kind_of_order["kind_of"]
+                if kind_selected == "Go back":
+                    break
+                check_kind_of_order(kind_selected)
 
         if answer == "See your previous orders":
             while True:
